99f7dd7d5c63e16edb8a323de3b5ab588c4c0d51
Abhishek kumar <aky8507049610@gmail.com>
2022-02-06 09:27:37 +0530

    added views

--- NodeJs/Project/crud app/index.js
+++ NodeJs/Project/crud app/index.js
@@ -1,12 +1,37 @@
+// All required modules
 const express = require("express")
 const dotenv = require('dotenv')
 const morgan = require("morgan")
-dotenv.config({path:"config.env"})
+const bodyParser = require('body-parser')
+const path = require("path")
+    
 const app = express()
-
-const PORT = process.env.PORT ||8080
+            
+// Port setup
+dotenv.config({path:"config.env"})
+const PORT = process.env.PORT ||8080 
+   
+// All midlewares
+app.use(morgan('tiny'))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use('/css',express.static(path.resolve(__dirname,'assets/css')))
+app.use('/img',express.static(path.resolve(__dirname,'assets/img')))
+app.use('/js',express.static(path.resolve(__dirname,'assets/js')))
+  
+      
+//view engine 
+app.set('view engine', 'ejs')
+// app.set('views',path.resolve(__dirname,'views'))
+ 
 app.get('',(req,res)=>{
-    res.send("crud app")
+    res.render('index',{ title: "Crud"})
 })
+app.get('/add-user',(req,res)=>{
+    res.render('add-user',{ title: "Crud"})
+})
+app.get('/update-user',(req,res)=>{
+    res.render('update-user',{ title: "Crud"})
+})
+//    
 
-app.listen(PORT)
+app.listen(PORT)   
