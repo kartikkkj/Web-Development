7b57c0d3562aa871b870610387626c8b20126443
Abhishek kumar <aky8507049610@gmail.com>
2022-02-10 13:20:44 +0530

    Scrapping done

--- NodeJs/Project/scrapping1/index.js
+++ NodeJs/Project/scrapping1/index.js
@@ -1,40 +1,63 @@
+// all required modules
 const axios = require("axios");
 const cheerio = require("cheerio");
 const xlsx = require("xlsx");
+const fs = require("fs");
+const path = require("path");
 
-const url = "https://www.espncricinfo.com/series/ipl-2012-13-1210595";
-let count =0;
+//URL
+const year = "2012-13";
+const url = `https://www.espncricinfo.com/series/ipl-${year}-1210595`;
+
+//function call
+fetch1(url)
+  .then((res) => {
+    extractLink(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+//fetch data
 function fetch1(url) {
-  axios
-    .get(url)
-    .then((res) => {
-      if(count==0)
-      extractLink(res.data);
-      else if(count==1){
-        extractAllLink(res.data);
-      }else{
-        extractData(res.data);
-      }
-      count++;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
-fetch1(url);
 
 
+//to view all result
 function extractLink(html) {
+  const MainDir = path.join(__dirname, `IPL-${year}`);
+  dirCreater(MainDir);
   const $ = cheerio.load(html);
   let anchorElement = $('a[data-hover="View All Results"]');
   let link = anchorElement.attr("href");
   const fullLink = "https://www.espncricinfo.com" + link;
- fetch1(fullLink)
-  
+  fetch1(fullLink)
+    .then((res) => {
+      extractAllLink(res);
+      console.log("1")
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+    
 }
 
 
+// to check scoreCard
 function extractAllLink(html) {
   const $ = cheerio.load(html);
   let anchorElement = $('a[data-hover="Scorecard"]');
@@ -42,10 +65,19 @@ function extractAllLink(html) {
     const link = $(anchorElement[i]).attr("href");
     const fullLink = "https://www.espncricinfo.com" + link;
     fetch1(fullLink)
-   
+      .then((res) => {
+        extractData(res);
+        console.log("2")
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
+
 }
 
+
+//data from scoreCard
 function extractData(html) {
   const $ = cheerio.load(html);
   const descEle = $(".match-header-container .description");
@@ -53,7 +85,6 @@ function extractData(html) {
   const vanue = descEle.text().split(",")[1].trim();
   const date = descEle.text().split(",")[2].trim();
   const inning = $(".match-scorecard-page .Collapsible");
-  const arr = [];
   for (let i = 0; i < inning.length; i++) {
     const teamName = $(inning[i]).find("h5").text().split("INNINGS")[0].trim();
     const opponentIndex = i == 0 ? 1 : 0;
@@ -64,7 +95,7 @@ function extractData(html) {
       .trim();
 
     const allRows = $(inning[i]).find(".table.batsman tbody tr");
-    
+
     for (let j = 0; j < allRows.length; j++) {
       const allCols = $(allRows[j]).find("td");
       if ($(allCols[0]).hasClass("batsman-cell")) {
@@ -74,35 +105,84 @@ function extractData(html) {
         const four = $(allCols[5]).text().trim();
         const sixes = $(allCols[6]).text().trim();
         const sr = $(allCols[7]).text().trim();
-        
-        const tempArr = []
-        tempArr["Player Name"] =PlayerName;
-        tempArr["Run"] =run
-        tempArr["Balls"]=balls
-        tempArr['Fours']=four
-        tempArr["Sixes"]==sixes
-        tempArr["Strike Rate"]=sr
-        arr.push(tempArr);
+
+        processPlayer(
+          teamName,
+          PlayerName,
+          run,
+          balls,
+          four,
+          sixes,
+          sr,
+          opponentName,
+          vanue,
+          date,
+          result
+        );
       }
     }
-
-    
   }
-  console.table(arr);
+}
+
+
+// file working function
+function processPlayer(
+  teamName,
+  PlayerName,
+  run,
+  balls,
+  four,
+  sixes,
+  sr,
+  opponentName,
+  vanue,
+  date,
+  result
+) {
+  const teamPath = path.join(__dirname, `IPL-${year}`, teamName);
+  dirCreater(teamPath)
+  const filePath = path.join(teamPath, `${PlayerName}.xlsx`);
+  const content = excelReader(filePath, PlayerName);
+  let PlayerObj = {
+    "TEAM" : teamName,
+    "Player Name" : PlayerName,
+    "Run":run,
+    "Balls":balls,
+    "Fours":four,
+    "Sixes":sixes,
+    "Strike Rate": sr,
+    "Opponent Name":opponentName,
+    "Venue":vanue,
+    "Date":date,
+    "Result":result,
+  };
+  content.push(PlayerObj)
+  excelWriter(filePath, content);
 }
 
 
 
 
-
-
-
-
-function excelWriter(filePath , json, sheetName){
-  const NewWB = xlsx.utils.book_new()
-  const newWS = xlsx.utils.json_to_sheet(json)
-  xlsx.utils.book_append_sheet(newWB,newWS,sheetName)
-  xlsx.writeFile(newWB,filePath)
+//utility functions
+function excelWriter(filePath, json) {
+  const newWB = xlsx.utils.book_new();
+  const newWS = xlsx.utils.json_to_sheet(json);
+  xlsx.utils.book_append_sheet(newWB, newWS, "All_Scores");
+  xlsx.writeFile(newWB, filePath);
 }
 
-function ex
+function excelReader(filePath, sheetName) {
+  if (!fs.existsSync(filePath)) {
+    return [];
+  }
+  const wb = xlsx.readFile(filePath);
+  const excelData = wb.Sheets(sheetName);
+  const ans = xlsx.utils.sheet_to_json(excelData);
+  return ans;
+}
+
+function dirCreater(filePath) {
+  if (!fs.existsSync(filePath)) {
+    fs.mkdirSync(filePath);
+  }
+}
