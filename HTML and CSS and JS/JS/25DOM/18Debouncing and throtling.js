59d1f9d5d3a97866d615e56cf4ed2102be8d0871
Abhishek kumar <aky8507049610@gmail.com>
2022-09-04 16:32:42 +0530

    web-changes

--- HTML and CSS and JS/JS/25DOM/18Debouncing and throtling.js
+++ HTML and CSS and JS/JS/25DOM/18Debouncing and throtling.js
@@ -7,14 +7,13 @@ function debounce(work, delay){// Debouncing
     let timerId
     return function optimizeFn(){
         clearTimeout(timerId)
-        timeEnd = setTimeout(()=>{
+        timerId = setTimeout(()=>{
             work()
         },delay)
     }
 }
 function throtling(work, delay){ // throtling
    let flag = true
-    
     return function optfn(){
         if(flag){
                 work()
--- ReactJs/React_Practice/project/Phone-Web-Integration/android-connection/src/Componenets/NavBar/NavBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./NavBar.module.css"
-export default function NavBar() {
-  return (
-    <>
-      <div className={styles.container}>NavBar</div>
-    </>
-  );
-}
